7394bdea9829c09d1c42421c4f5c348eca577aaf
Paulo Victor <spaulovictor813@gmail.com>
2025-10-05 19:04:54 -0300

    construindo a pagina de admin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import TanStackProvider from "@/components/providers/TanStackProvider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,26 +27,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        variables: {
-          colorPrimary: "#e78a53",
-          colorBackground: "#f3f4f6",
-          colorText: "#111827",
-          colorTextSecondary: "#6b7280",
-          colorInputBackground: "#f3f4f6",
-        },
-      }}
-    >
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
-        >
-          {/* this is done in the home page component */}
-          {/* <UserSync /> */}
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <TanStackProvider>
+      <ClerkProvider
+        appearance={{
+          variables: {
+            colorPrimary: "#e78a53",
+            colorBackground: "#f3f4f6",
+            colorText: "#111827",
+            colorTextSecondary: "#6b7280",
+            colorInputBackground: "#f3f4f6",
+          },
+        }}
+      >
+        <html lang="en">
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+          >
+            {/* this is done in the home page component */}
+            {/* <UserSync /> */}
+            <Toaster />
+            {children}
+          </body>
+        </html>
+      </ClerkProvider>
+    </TanStackProvider>
   );
 }
---
+++ src/components/admin/AddDoctorDialog.tsx
@@ -0,0 +1,182 @@
+import { useCreateDoctor } from "@/hooks/use-doctors";
+import { Gender } from "@prisma/client";
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Button } from "../ui/button";
+import { formatPhoneNumber } from "@/lib/utils";
+
+interface AddDoctorDialogProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+function AddDoctorDialog({ isOpen, onClose }: AddDoctorDialogProps) {
+  const [newDoctor, setNewDoctor] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    speciality: "",
+    gender: "MALE" as Gender,
+    isActive: true,
+  });
+
+  const createDoctorMutation = useCreateDoctor();
+
+  const handlePhoneChange = (value: string) => {
+    const formattedPhoneNumber = formatPhoneNumber(value);
+    setNewDoctor({ ...newDoctor, phone: formattedPhoneNumber });
+  };
+
+  const handleSave = () => {
+    createDoctorMutation.mutate({ ...newDoctor }, { onSuccess: handleClose });
+  };
+
+  const handleClose = () => {
+    onClose();
+    setNewDoctor({
+      name: "",
+      email: "",
+      phone: "",
+      speciality: "",
+      gender: "MALE",
+      isActive: true,
+    });
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={handleClose}>
+      <DialogContent className="sm:max-w-[500px]">
+        <DialogHeader>
+          <DialogTitle>Adicionar Novo Dentista </DialogTitle>
+          <DialogDescription>
+            Adicione um novo dentista ao sistema
+          </DialogDescription>
+        </DialogHeader>
+
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="space-y-2">
+              <Label htmlFor="new-name">Nome *</Label>
+              <Input
+                id="new-name"
+                value={newDoctor.name}
+                onChange={(e) =>
+                  setNewDoctor({ ...newDoctor, name: e.target.value })
+                }
+                placeholder="Dr. John Smith"
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="new-speciality">Especialidade *</Label>
+              <Input
+                id="new-speciality"
+                value={newDoctor.speciality}
+                onChange={(e) =>
+                  setNewDoctor({ ...newDoctor, speciality: e.target.value })
+                }
+                placeholder="General Dentistry"
+              />
+            </div>
+          </div>
+
+          <div className="space-y-2">
+            <Label htmlFor="new-email">Email *</Label>
+            <Input
+              id="new-email"
+              type="email"
+              value={newDoctor.email}
+              onChange={(e) =>
+                setNewDoctor({ ...newDoctor, email: e.target.value })
+              }
+              placeholder="doctor@example.com"
+            />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="new-phone">Telefone</Label>
+            <Input
+              id="new-phone"
+              value={newDoctor.phone}
+              onChange={(e) => handlePhoneChange(e.target.value)}
+              placeholder="(555) 123-4567"
+            />
+          </div>
+
+          <div className="grid grid-cols-2 gap-4">
+            <div className="space-y-2">
+              <Label htmlFor="new-gender">Gênero</Label>
+              <Select
+                value={newDoctor.gender || ""}
+                onValueChange={(value) =>
+                  setNewDoctor({ ...newDoctor, gender: value as Gender })
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select gender" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="MALE">Masculino</SelectItem>
+                  <SelectItem value="FEMALE">Feminino</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="new-status">Status</Label>
+              <Select
+                value={newDoctor.isActive ? "active" : "inactive"}
+                onValueChange={(value) =>
+                  setNewDoctor({ ...newDoctor, isActive: value === "active" })
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="active">Ativo</SelectItem>
+                  <SelectItem value="inactive">Inativo</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </div>
+
+        <DialogFooter>
+          <Button variant="outline" onClick={handleClose}>
+            Cancelar
+          </Button>
+
+          <Button
+            onClick={handleSave}
+            className="bg-primary hover:bg-primary/90"
+            disabled={
+              !newDoctor.name ||
+              !newDoctor.email ||
+              !newDoctor.speciality ||
+              createDoctorMutation.isPending
+            }
+          >
+            {createDoctorMutation.isPending ? "Adding..." : "Add Doctor"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default AddDoctorDialog;
